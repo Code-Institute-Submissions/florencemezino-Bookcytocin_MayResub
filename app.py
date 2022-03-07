8ba1280140951cc08243193eb89a2106635b0836
Florence Mezino <florence.mezino@outlook.com>
2022-03-07 21:19:11 +0000

    Added templates to add and edit goal and review in mybooklog profile user, added functions add , edit , delete for goal and review, added review in community template

--- app.py
+++ app.py
@@ -51,34 +51,12 @@ def collections():
         "collections.html", page_title="Collections", books=books)
 
 
-# Community : display community / bog
+# Community : display community / blog
 @app.route("/community")
 def community():
     users = list(mongo.db.users.find().limit(8))
     return render_template(
         "community.html", page_title="The Bookcytocin Club", user=users)
-
-
-# MyBookLog : user can submit a review
-@app.route("/review", methods=["POST"])
-def review():
-    username = session["user"]
-    user = mongo.db.users.find_one({"username": username})
-    if request.method == "POST":
-        data = {
-            "review_title": request.form.get("review_title"),
-            "review_author": request.form.get("review_author"),
-            "review_content": request.form.get("review_content"),
-            "review_full_name": request.form.get("review_full_name"),
-        }
-        print("My reading goal", user)
-        print(data)
-        flash("Your review is being processed. Stay tune!")
-        mongo.db.users.update_one(
-            {"username": session["user"]}, {"$set": data})
-
-        print("Add a review", user)
-    return render_template("mybooklog.html", page_title="MyBookLog", user=user)
 
 
 # Sign up
@@ -132,8 +110,28 @@ def login():
     return render_template("login.html", page_title="Login")
 
 
-# MyBookLog : user save goal commitment
-@app.route("/mybooklog", methods=["POST", "GET"])
+# MyBookLog : view
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("mybooklog.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+# Logout
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+# MyBookLog : user add a goal
+@app.route("/mybooklog/add_goal", methods=["POST", "GET"])
 def mybooklog():
     username = session["user"]
     user = mongo.db.users.find_one({"username": username})
@@ -147,7 +145,7 @@ def mybooklog():
 
         print("My reading goal", user)
         print(dta)
-        flash("Your commitment has been saved!")
+        flash("Your goal has been saved!")
         mongo.db.users.update_one({"username": session["user"]}, {"$set": dta})
         return render_template(
             "mybooklog.html", page_title="MyBookLog", user=user)
@@ -155,12 +153,75 @@ def mybooklog():
     return render_template("mybooklog.html", page_title="MyBookLog", user=user)
 
 
-# Logout
-@app.route("/logout")
-def logout():
-    flash("You have been logged out")
-    session.pop("user")
-    return redirect(url_for("login"))
+# MyBookLog : user view / edit goal 
+@app.route("/mybooklog/edit_goal/<goal_id>", methods=["GET", "POST"])
+def edit_goal(goal_id):
+    if request.method == "POST":
+        is_goal = "on" if request.form.get("is_goal") else "off"
+        submit = {
+            "goal_reason": request.form.get("goal_reason"),
+            "goal_obstacle": request.form.get("goal_obstacle"),
+            "goal_email": request.form.get("goal_email"),
+            "goal_signature": request.form.get("goal_signature"),
+        }
+        mongo.db.goals.update({"_id": ObjectId(goal_id)}, submit)
+        flash("Goal Successfully Updated")
+
+    goal = mongo.db.tasks.find_one({"_id": ObjectId(goal_id)})
+    return render_template("edit_goal.html", goal=goal)
+
+# MyBookLog : user view / delete goal
+@app.route("/delete_goal/<goal_id>")
+def delete_goal(goal_id):
+    mongo.db.goals.remove({"_id": ObjectId(goal_id)})
+    flash("Goal Successfully Deleted")
+    return redirect(url_for("add_goal"))
+
+
+# MyBookLog : user add a review
+@app.route("/add_review", methods=["POST"])
+def review():
+    username = session["user"]
+    user = mongo.db.users.find_one({"username": username})
+    if request.method == "POST":
+        data = {
+            "review_title": request.form.get("review_title"),
+            "review_author": request.form.get("review_author"),
+            "review_content": request.form.get("review_content"),
+            "review_full_name": request.form.get("review_full_name"),
+        }
+        print("My reading goal", user)
+        print(data)
+        flash("Your review is being processed. Stay tune!")
+        mongo.db.users.update_one(
+            {"username": session["user"]}, {"$set": data})
+
+        print("Add a review", user)
+    return render_template("mybooklog.html", page_title="MyBookLog", user=user)
+
+
+# MyBookLog : user view / edit review
+@app.route("/mybooklog/edit_review/<review_id>", methods=["GET", "POST"])
+def edit_review(review_id):
+    if request.method == "POST":
+        is_review = "on" if request.form.get("is_review") else "off"
+        submit = {
+            "review_book": request.form.get("review_book"),
+            "review_content": request.form.get("review_content"),
+            "review_full_name": request.form.get("review_full_name"),
+        }
+        mongo.db.goals.update({"_id": ObjectId(review_id)}, submit)
+        flash("Review Successfully Updated")
+
+    review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
+    return render_template("edit_review.html", review=review)
+
+
+# MyBookLog : user view / delete review
+def delete_review(review_id):
+    mongo.db.reviews.remove({"_id": ObjectId(review_id)})
+    flash("Review Successfully Deleted")
+    return redirect(url_for("add_review"))
 
 
 if __name__ == "__main__":
