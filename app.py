f5529af75980f6f610be2c4b59400a626ec8a748
Florence Mezino <florence.mezino@outlook.com>
2022-04-28 17:03:00 +0000

    Fixing function to remove book from user wishlist

--- app.py
+++ app.py
@@ -23,20 +23,25 @@ def index():
     return render_template("bookcytocin.html", page_title="Bookcytocin")
 
 
-# About Bookcytocin
 @app.route("/bookcytocin")
 def about():
+    """
+    Display home page / about Bookcytocin
+    """
     return render_template("bookcytocin.html", page_title="Bookcytocin")
 
 
-# Readflix : find 4 books in one collection and save book to wishlist
 @app.route("/readflix", methods=["GET", "POST"])
 def readflix():
+    """
+    Display books in readflix view and save book to user's wishlist
+    """
     if request.method == "POST":
         book_id = request.form.get("book_id")
         book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
 
         user_saved_book = {
+                "_id": book_id,
                 "book_image_url": book["image_url"],
                 "book_title": book["title"],
                 "book_author": book["author"],
@@ -47,6 +52,9 @@ def readflix():
         flash("Book Successfully Saved in your Wishlist!")
         user = mongo.db.users.update_one({"username": session["user"]}, {
             "$push": {"saved_books": user_saved_book}})
+
+        user = mongo.db.users.find_one({"username": session["user"]})
+        print(user)
         return render_template(
             "index.html", page_title="Readflix", user=user)  
 
@@ -56,14 +64,17 @@ def readflix():
         "index.html", page_title="Readflix", books=books)
 
 
-# Collection : display books and save book to wishlist
 @app.route("/collections", methods=["GET", "POST"])
 def collections():
+    """
+    Display books in collections view and save book to user's wishlist
+    """
     if request.method == "POST":
         book_id = request.form.get("book_id")
         book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
 
         user_saved_book = {
+                "_id": book_id,
                 "book_image_url": book["image_url"],
                 "book_title": book["title"],
                 "book_author": book["author"],
@@ -74,6 +85,9 @@ def collections():
         flash("Book Successfully Saved in your Wishlist!")
         user = mongo.db.users.update_one({"username": session["user"]}, {
             "$push": {"saved_books": user_saved_book}})
+
+        user = mongo.db.users.find_one({"username": session["user"]})
+        print(user)    
         return render_template(
             "collections.html", page_title="Collections", user=user)
 
@@ -84,9 +98,11 @@ def collections():
         collections=collections)
 
 
-# Collection : display books by collection
 @app.route("/get_collections/<collection_name>", methods=["GET", "POST"])
 def get_collections(collection_name):
+    """
+    Display books by collection
+    """
     collections = list(mongo.db.collections.find())
     books = list(mongo.db.books.find({"collection_name": collection_name}))
     return render_template(
@@ -94,46 +110,54 @@ def get_collections(collection_name):
         collections=collections, books=books)
 
 
-# Delete books from wishlist
 @app.route("/delete_saved_book/<book_id>", methods=["GET", "POST"])
 def delete_saved_book(book_id):
-    mongo.db.books.remove({"_id": ObjectId(book_id)})
+    """
+    Remove book from user's wishlist
+    """
+    mongo.db.users.update({"username": session["user"]}, {"$pull": {
+        "user.saved_books": {"_id": ObjectId(book_id)}}})
     flash("Book Successfully Removed from your Wishlist")
 
     username = session['user']
-    books = user["saved_books"]
-    return redirect(url_for("profile", username=username, book=book))
+    return redirect(url_for("profile", username=username))
 
 
-# Collection : find a book via search bar
 @app.route("/search", methods=["GET", "POST"])
 def search():
+    """
+    Find a book via search bar
+    """
     query = request.form.get("query")
     books = list(mongo.db.books.find({"$text": {"$search": query}}))
     return render_template(
         "collections.html", page_title="Collections", books=books)
 
 
-# Community : display community / blog
 @app.route("/community")
 def community():
+    """
+    Display community / blog
+    """
     users = list(mongo.db.users.find().limit(6))
     return render_template(
         "community.html", page_title="The Bookcytocin Club", user=users)
 
 
-# MyBookLog (Add / Update goal)
 @app.route("/mybooklog/<username>", methods=["GET", "POST"])
 def profile(username):
+    """
+    Update user's goal
+    """
     user = mongo.db.users.find_one(
         {"username": session["user"]})
     if user:
+        books = user["saved_books"]
         if request.method == "POST":
             goal = {
                 "goal_level": request.form.get("goal_level"),
                 "goal_reason": request.form.get("goal_reason"),
                 "goal_obstacle": request.form.get("goal_obstacle"),
-                "goal_email": request.form.get("goal_email"),
                 "goal_signature": request.form.get("goal_signature"),
             }
 
@@ -142,18 +166,20 @@ def profile(username):
                 {"username": session["user"]}, {"$set": goal})
             user = mongo.db.users.find_one({"username": session["user"]})
             return render_template(
-                "mybooklog.html", page_title="MyBookLog", user=user)
+                "mybooklog.html", page_title="MyBookLog", user=user,
+                books=books)
 
-        books = user["saved_books"]
         return render_template("mybooklog.html", user=user, books=books)
     return redirect(url_for("profile", username=username))
 
 # MyBookLog (Delete goal)
 
 
-# Sign up
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    """
+    Allow user to sign up
+    """
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username")})
@@ -177,9 +203,11 @@ def signup():
     return render_template("signup.html", page_title="Sign up")
 
 
-# Log in
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Allow user to log in
+    """
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
@@ -203,9 +231,11 @@ def login():
     return render_template("login.html", page_title="Login")
 
 
-# Logout
 @app.route("/logout")
 def logout():
+    """
+    Allow user to log out
+    """
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
